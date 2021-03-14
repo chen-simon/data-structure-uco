024c1244df6edd6518aac76555e2c962558c4d3f
narwhalblast <ognomisch@gmail.com>
2021-03-13 19:33:26 -0500

    Singly Linked List (len and str)
    And so it begins.

---
+++ linked lists/singly_linked_list.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from typing import Any, Optional
+
+class _Node:
+    """ A node in a singly linked list.
+    """
+    item: Any
+    next: Optional[_Node]
+
+    def __init__(self, item: Any, next: Optional[_Node] = None) -> None:
+        self.item = item
+        self.next = next
+
+
+class SinglyLinkedList:
+    """ A singly linked list.
+    """
+    _first: Optional[_Node]
+    _size: int  # To prevent worst case Theta(n) size calls
+
+    def __init__(self, lst: Optional[list[Any]] = None) -> None:
+        if lst is None or lst == []:  # If lst is empty
+            self._size = 0
+            self._first = None
+        else:
+            self._size = 1
+            self._first = _Node(lst[0])
+
+            curr = self._first
+            for i in range(1, len(lst)):
+                new_node = _Node(lst[i])
+                curr.next = new_node
+                self._size += 1
+                curr = new_node
+        return
+
+    def __str__(self) -> str:
+        string_so_far = '['
+        curr = self._first
+
+        while curr is not None:
+            string_so_far += str(curr.item)
+            if curr.next is not None:
+                string_so_far += ', '
+            curr = curr.next
+
+        string_so_far += ']'
+        return string_so_far
