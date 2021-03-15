f7ced8fef38ecb2e091990049b21ff04e43993a4
narwhalblast <ognomisch@gmail.com>
2021-03-15 12:56:55 -0400

    Update Queue

--- lists/queue.py
+++ lists/queue.py
@@ -2,14 +2,15 @@ from __future__ import annotations
 from typing import Any
 from singly_linked_list import SinglyLinkedList
 
+
 class Queue:
     """ A queue.
     """
-    _values: SinglyLinkedList[Any]  # The start of _values is the front of the queue
+    _values: SinglyLinkedList  # The start of _values is the front of the queue
     _size: int  # To remove reliance on len(_values)
 
     def __init__(self):
-        self._values = []
+        self._values = SinglyLinkedList()
         self._size = 0
 
     def is_empty(self) -> bool:
